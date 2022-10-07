cc2b118ab04e53fd6ba696920b2eefcd01d5b0dd
IgalTam <igal.tamarkin@gmail.com>
2022-10-07 13:30:04 -0700

    passed step 6 of TDD assignment

--- portfolio.js
+++ portfolio.js
@@ -1,125 +1,52 @@
 const express = require('express');
-// const cors = require('cors');
-// const { v4: uuidv4 } = require('uuid');
 const app = express();
-// const port = 5000;
 
-// //debug: set DEBUG=express:router
-
-// app.use(cors());
 app.use(express.json())
 
-// app.get('/', (req,res) => {
-//     res.send('Hello, world!');
-// });
-
-// app.get('/users', (req, res) => {
-//     const name = req.query.name;
-//     const job = req.query.job;
-//     if (name != undefined && job != undefined) {
-//         let resultN = findUserByName(name);
-//         let resultJ = findUserByJob(job);
-//         let resultCom = resultN.filter(job => resultJ.includes(job))
-//         resultCom = {users_list: resultCom};
-//         res.send(resultCom);
-//     } else if (name != undefined) {
-//         let result = findUserByName(name);
-//         result = {users_list: result};
-//         res.send(result);
-//     } else if (job != undefined) {
-//         let result = findUserByJob(job);
-//         result = {users_list: result};
-//         res.send(result);
-//     } else {
-//         res.send(users);
-//         //res.status(404).end();
-//     }
-    
-// });
-
-// const findUserByName = (name) => {
-//     return users['users_list'].filter( (user) => user['name'] === name);
-// }
-
-// const findUserByJob = (job) => {
-//     return users['users_list'].filter( (user) => user['job'] === job);
-// }
-
-// app.get('/users/:id', (req, res) => {
-//     const id = req.params['id'];
-//     let result = findUserById(id);
-//     if (result === undefined || result.length == 0)
-//         res.status(404).send('Resource not found.');
-//     else {
-//         result = {users_list: result};
-//         res.send(result);
-//     }
-// });
-
-// function findUserById(id) {
-//     return users['users_list'].find( (user) => user['id'] === id);
-//     // or: return users['users_list'].filter( (user) => user['id'] === id);
-// }
-
-// app.post('/users', (req, res) => {
-//     const userToAdd = req.body;
-//     addUser(userToAdd);
-//     res.status(201).send(userToAdd); 
-// });
-
-// function addUser(user) {
-//     user.id = uuidv4();
-//     users['users_list'].push(user);
-// }
-
-// app.delete('/users/:id', (req, res) => {
-//     const id = req.params['id'];
-//     let result = findUserById(id);
-//     if (result === undefined || result.length == 0)
-//         res.status(404).send('Resource not found.');
-//     else {
-//         users['users_list'].splice(users['users_list'].indexOf(result), 1);
-//         res.status(204).end();
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`);
-// });
 
 exports.initPortfolio = function initPortfolio () {
-    return portfolio;
+    return new portfolio();
 }
 
-const portfolio = {}
+class portfolio {
+    portfl_lst;
+    constructor() {
+        this.portfl_lst = [];
+    }
 
-// const users = { 
-//     users_list :
-//     [
-//        { 
-//           id : 'xyz789',
-//           name : 'Charlie',
-//           job: 'Janitor',
-//        },
-//        {
-//           id : 'abc123', 
-//           name: 'Mac',
-//           job: 'Bouncer',
-//        },
-//        {
-//           id : 'ppp222', 
-//           name: 'Mac',
-//           job: 'Professor',
-//        }, 
-//        {
-//           id: 'yat999', 
-//           name: 'Dee',
-//           job: 'Aspring actress',
-//        },
-//        {
-//           id: 'zap555', 
-//           name: 'Dennis',
-//           job: 'Bartender',
-//        }
-//     ]
-//  }
+    isEmpty() {
+        if(this.portfl_lst.length === 0)
+            return true;
+        return false;
+    }
+
+    tickerCount() {
+        return this.portfl_lst.length;
+    }
+
+    addTicker(tickerName, shares) {
+        this.portfl_lst.push({"tickerName": tickerName, "tickerCount": shares});
+    }
+
+    makePurchase(tickername, shares) {
+        if(tickername === undefined)
+            this.addTicker(tickername, tickerCount);
+        else
+            this.getTicker(tickername).tickerCount += shares;
+    }
+
+    makeSale(tickername, shares) {
+        if(tickername === undefined)
+            this.addTicker(tickername, tickerCount);
+        else
+            this.getTicker(tickername).tickerCount -= shares;
+    }
+
+    getTicker(tickerName) {
+        return this.portfl_lst.find(element => element.tickerName === tickerName);
+    }
+
+    getShares(tickerName) {
+        return this.portfl_lst.find(element => element.tickerName === tickerName).tickerCount;
+    }
+}
--- portfolio.test.js
+++ portfolio.test.js
@@ -5,5 +5,40 @@ beforeEach(() => {
 });
 
 test('Empty portfolio', () => {
-    expect(portfol.length === 0).toBeFalsy();
+    expect(portfol.portfl_lst.length === 0).toBeTruthy();
+});
+
+test('Empty portfolio responds', () => {
+    expect(portfol.isEmpty()).toBeTruthy();
+});
+
+test('Get ticker count', () => {
+    const target = 2;
+    portfol.addTicker("GME", 5);
+    portfol.addTicker("RBLX", 10);
+    const result = portfol.tickerCount();
+    expect(target).toBe(result);
+});
+
+test('Make purchase', () => {
+    const target = 7;
+    portfol.addTicker("GME", 5);
+    portfol.makePurchase("GME", 2);
+    const result = portfol.getTicker("GME").tickerCount;
+    expect(target).toBe(result);
+});
+
+test('Make sale', () => {
+    const target = 3;
+    portfol.addTicker("GME", 5);
+    portfol.makeSale("GME", 2);
+    const result = portfol.getTicker("GME").tickerCount;
+    expect(target).toBe(result);
+});
+
+test('Declare shares', () => {
+    const target = 5;
+    portfol.addTicker("GME", 5);
+    const result = portfol.getShares("GME");
+    expect(target).toBe(result);
 });
